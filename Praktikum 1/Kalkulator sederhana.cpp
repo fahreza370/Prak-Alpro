c5ece7be2e094d2871bffc44252691159fea4f5a
fahreza370 <101689358+fahreza370@users.noreply.github.com>
2022-03-19 16:31:22 +0700

    Update Kalkulator sederhana.cpp

--- Praktikum 1/Kalkulator sederhana.cpp
+++ Praktikum 1/Kalkulator sederhana.cpp
@@ -4,10 +4,10 @@ main(){
 	float bil1,bil2,hasil;
 	int pil;
 	cout<<"===========KALKULATOR=============="<<endl;
-	cout<<"1. Tambah"<<endl;
-	cout<<"2. Kurang"<<endl;
-	cout<<"3. Kali"<<endl;
-	cout<<"4. Pembagian"<<endl;
+	cout<<"|1. Tambah                        |"<<endl;
+	cout<<"|2. Kurang                        |"<<endl;
+	cout<<"|3. Kali                          |"<<endl;
+	cout<<"|4. Pembagian                     |"<<endl;
 	cout<<"Pilih : ";
 	cin>>pil;
 	cout<<"===================================="<<endl;
